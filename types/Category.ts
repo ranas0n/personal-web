8c26ce349dbf1e51d6b8980905b066e244953a75
BuildTools <nasutionrachmat@gmail.com>
2024-12-04 19:26:00 +0700

    trying out modularizing the types

---
+++ types/Category.ts
@@ -0,0 +1,8 @@
+export type Category =
+	| "web"
+	| "machinelearning"
+	| "deeplearning"
+	| "dataanalysis"
+	| "iot"
+	| "desktop"
+	| "mobile";
---
+++ types/Project.ts
@@ -0,0 +1,9 @@
+export type Project = {
+	proj_id: number;
+	proj_name: string;
+	proj_img: string;
+	github: string;
+	hosting: string;
+	category: string;
+	description: string;
+};
